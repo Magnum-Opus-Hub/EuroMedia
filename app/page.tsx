647c2c5f838059e70818da0acf6f44fc3cab2014
FilipTheGod <filipstan666@gmail.com>
2023-07-31 14:27:59 +0300

    first push

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Image from 'next/image'
+import Navbar from '@/components/navbar'
+
+export default function Home() {
+  return (
+    <div>
+      <Navbar />
+      <h1>Home</h1>
+      </div>
+  )
+}
