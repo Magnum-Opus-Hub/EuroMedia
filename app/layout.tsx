525c045fd6b33dcd961bec6cb0d63accc14d5c23
FilipTheGod <filipstan666@gmail.com>
2023-11-08 14:15:28 -0500

    Gata Routing 4 vercel bug build

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import { Analytics } from '@vercel/analytics/react';
-import Navbar from '@/components/Navbar';
+import Navbar from '../components/Navbar';
 
 const inter = Inter({ subsets: ['latin'] })
 
