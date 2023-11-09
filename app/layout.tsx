b58d777d43954c9d829a60d39705267fcde8698e
FilipTheGod <filipstan666@gmail.com>
2023-11-09 14:32:38 -0500

    try 8

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import { Inter } from 'next/font/google'
 import { Analytics } from '@vercel/analytics/react';
 import Navbar from '@/components/Navbar';
 import Footer from '@/components/Footer';
-
+ 
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
